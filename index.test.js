7d2dd17bb48b55d08635df01c6e1ba1c7fe33c7a
DavidGonzalez916 <dagonzalez@deloitte.com>
2024-03-20 16:48:37 -0400

    Committing all classes and tests complete

--- index.test.js
+++ index.test.js
@@ -14,31 +14,64 @@ describe('Band, Musician, and Song Models', () => {
 
     test('can create a Band', async () => {
         // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const nirvana = await Band.create({name: "Nirvana", genre:"Alternative Rock"});       
+        expect(nirvana.name).toBe("Nirvana");
+        expect(nirvana.genre).toBe("Alternative Rock");
     })
 
     test('can create a Musician', async () => {
         // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const hayley = await Musician.create({name:"Hayley", instrument:"Voice"});
+        expect(hayley.name).toBe('Hayley');
+        expect(hayley.instrument).toBe("Voice");
     })
+
+    test('can create a Song', async () =>{
+        const zombie = await Song.create({title:"Zombie", year:1994, length: 306 });
+        expect(zombie.title).toBe("Zombie");
+        expect(zombie.year).toBe(1994);
+        expect(zombie.length).toBe(306);
+    });
 
     test('can update a Band', async () => {
         // TODO - test updating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const nirvana = await Band.findByPk(1);
+        const paramore = await nirvana.update({name: "Paramore"});
+        expect(paramore.name).toBe("Paramore");
     })
 
     test('can update a Musician', async () => {
-        // TODO - test updating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        // TODO - test updating a 
+        const hayley = await Musician.findByPk(1)
+        const updatedHayley = await hayley.update({instrument: "Guitar"});
+        expect(updatedHayley.instrument).toBe('Guitar');
+    })
+
+    test('can update a song', async () =>{
+        const zombie = await Song.findByPk(1)
+        const still = await zombie.update({title:"Still Into You"});
+        expect(still.title).toBe("Still Into You");
+        
     })
 
     test('can delete a Band', async () => {
-        // TODO - test deleting a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        let nirvana = await Band.findByPk(1)
+        await nirvana.destroy();
+        const destroyed = await Band.findByPk(1);
+        expect(destroyed).toBe(null);
     })
 
     test('can delete a Musician', async () => {
         // TODO - test deleting a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const hayley = await Musician.findByPk(1);
+        await hayley.destroy();
+        const destroyed = await Musician.findByPk(1);
+        expect(destroyed).toBe(null);
+    })
+    test('can delete a song', async () => {
+        const zombie = await Song.findByPk(1);
+        await zombie.destroy();
+        const destroyed = await Song.findByPk(1);
+        expect(destroyed).toBe(null);
     })
 })
--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,11 @@
-const {Sequelize, sequelize} = require('../db');
+const {sequelize, DataTypes} = require('../db');
 
-// TODO - define the Musician model
-let Musician;
+// TODO - define the Musician mode
+const Musician = sequelize.define('Musician', {
+    name: DataTypes.STRING,
+    instrument: DataTypes.STRING
+});
+
 
 module.exports = {
     Musician
