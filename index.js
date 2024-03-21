98b4734302069b88ce5c70686399906b46a4e4d6
Lea Iodice <liodice@deloitte.com>
2024-03-21 16:43:17 -0400

    completed day 2

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const { Band } = require('./models/Band')
 const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
+// band and muscian: one to many
+Band.hasMany(Musician);
+Musician.belongsTo(Band); 
+
+// band to song many to many 
+Band.belongsToMany(Song, {through: "BandtoSong"});
+Song.belongsToMany(Band, {through: "BandtoSong"}); 
 
 
 
